98eec1aad710421c5d341d6fca8c22eba81c6a8b
adamya-singh <7adamyasingh@gmail.com>
2023-07-22 02:11:50 -0400

    work on add-student php

--- add-student.php
+++ add-student.php
@@ -1,11 +1,7 @@
 <?php
 
-if (!empty("full_name")) {
-    $full_name = $_POST["full_name"];
-}
-if (!empty("level")) {
-    $level = $_POST["level"];
-}
+$full_name=$_POST["full_name"];
+$level=$_POST["level"];
 
 var_dump($full_name, $level);
 ?>
